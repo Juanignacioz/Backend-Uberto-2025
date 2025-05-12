3ac41210957cc153194c372a89d50e85e5de7a9c
marycairo <marycairo2@gmail.com>
2025-05-11 23:01:13 -0300

    Registro de click en choferes

--- src/main/kotlin/uberto/backendgrupo72025/repository/mongo/ClickLogsRepository.kt
+++ src/main/kotlin/uberto/backendgrupo72025/repository/mongo/ClickLogsRepository.kt
@@ -2,7 +2,7 @@ package uberto.backendgrupo72025.repository.mongo
 
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
-import uberto.backendgrupo72025.ClickLogs
+ import uberto.backendgrupo72025.ClickLogs
 
 @Repository
 interface ClickLogRepository : MongoRepository<ClickLogs, String>
