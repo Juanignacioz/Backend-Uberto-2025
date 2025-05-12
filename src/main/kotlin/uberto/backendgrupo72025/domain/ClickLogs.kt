47ee5ffa2fb6c550004141c602bbbd5905312e44
marycairo <marycairo2@gmail.com>
2025-05-11 22:43:14 -0300

    Registro de click en choferes

---
+++ src/main/kotlin/uberto/backendgrupo72025/domain/ClickLogs.kt
@@ -0,0 +1,13 @@
+package uberto.backendgrupo72025
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+
+@Document(collection = "click_logs")
+data class ClickLogs(
+    @Id
+    val id: String? = null,
+    val conductorNombre: String,
+    val usuarioId: String,
+    val fechaHora: LocalDateTime = LocalDateTime.now()
+)
---
+++ src/main/kotlin/uberto/backendgrupo72025/repository/mongo/ClickLogsRepository.kt
@@ -0,0 +1,8 @@
+package uberto.backendgrupo72025.repository.mongo
+
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+import uberto.backendgrupo72025.ClickLogs
+
+@Repository
+interface ClickLogRepository : MongoRepository<ClickLogs, String>
