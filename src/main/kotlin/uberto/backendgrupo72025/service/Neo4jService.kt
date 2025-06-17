2b44ff5093ae4143eb58718348d38e9b2f65e832
Juanignacioz <80764320+Juanignacioz@users.noreply.github.com>
2025-06-17 17:27:29 -0300

    se configura neo

--- src/main/kotlin/uberto/backendgrupo72025/service/Neo4jService.kt
+++ src/main/kotlin/uberto/backendgrupo72025/service/Neo4jService.kt
@@ -3,6 +3,7 @@ package uberto.backendgrupo72025.service
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uberto.backendgrupo72025.domain.NotFoundException
+import uberto.backendgrupo72025.domain.neo4j.ViajeRelation
 import uberto.backendgrupo72025.repository.neo4j.ConductorNodeRepository
 import uberto.backendgrupo72025.repository.neo4j.ViajeroNodeRepository
 import java.time.LocalDateTime
@@ -13,20 +14,35 @@ class Neo4jService(
     val conductorNodeRepository: ConductorNodeRepository,
 ) {
 
+//    @Transactional("neo4jTransactionManager")
+//    fun crearViaje(idViajero: String, idConductor: String?, fechafin: LocalDateTime) {
+//        val viajero = viajeroNodeRepository.findByViajeroId(idViajero)
+//            .orElseThrow { NotFoundException("No se encontro el nodo del viajero solicitado") }
+//        val conductor = conductorNodeRepository.findByConductorId(idConductor!!)
+//            .orElseThrow { NotFoundException("No se encontro el nodo del conductor solicitado") }
+//        viajeroNodeRepository.crearRelacionViaje(idViajero,idConductor,fechafin)
+////        val nuevoViaje = ViajeRelation(
+////            conductor = conductor,
+////            fechaDeFinalizacion = fechafin
+////        )
+////        viajero.agregarViaje(nuevoViaje)
+////        println("viajesssssssssss"+ viajero.viajes)
+////        viajeroNodeRepository.save(viajero)
+//    }
+
     @Transactional("neo4jTransactionManager")
     fun crearViaje(idViajero: String, idConductor: String?, fechafin: LocalDateTime) {
-        val viajero = viajeroNodeRepository.findByViajeroId(idViajero)
+        val viajeroNode = viajeroNodeRepository.findByViajeroId(idViajero)
             .orElseThrow { NotFoundException("No se encontro el nodo del viajero solicitado") }
-        val conductor = conductorNodeRepository.findByConductorId(idConductor!!)
+        val conductorNode = conductorNodeRepository.findByConductorId(idConductor!!)
             .orElseThrow { NotFoundException("No se encontro el nodo del conductor solicitado") }
-        viajeroNodeRepository.crearRelacionViaje(idViajero,idConductor,fechafin)
-//        val nuevoViaje = ViajeRelation(
-//            conductor = conductor,
-//            fechaDeFinalizacion = fechafin
-//        )
-//        viajero.agregarViaje(nuevoViaje)
-  //      println("viajesssssssssss"+ viajero.viajes)
-//        viajeroNodeRepository.save(viajero)
+//        viajeroNodeRepository.crearRelacionViaje(idViajero,idConductor,fechafin)
+        viajeroNode.agregarViaje(ViajeRelation(conductorNode, fechafin))
+        println("viajesssssssssss"+ viajeroNode.viajes)
+        println("#################################")
+        println("id de la relacion " + viajeroNode.viajes.size)
+
+        viajeroNodeRepository.save(viajeroNode)
     }
 
 }
